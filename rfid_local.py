ef5488ec716ad28f38ae4554fafcea2b1eebf197
DESKTOP-1NTIMEG\Julio <julio.cesar.vgs@gmail.com>
2018-10-27 11:37:21 -0300

    criacao de um arquivo local, agora ha identificacao personalizada :D

---
+++ rfid_local.py
@@ -0,0 +1,33 @@
+from builtins import tuple
+import MFRC522
+import RPi.GPIO as GPIO
+import time
+
+tags_liberadas = {
+    (42, 42, 228, 89, 189): 'Julio',
+    (70, 84, 191, 73, 228): 'Tiago'
+}
+
+LeitorRFID = MFRC522.MFRC522()
+
+print('Aproxime a TAG')
+
+while True:
+
+    # Verifica se existe TAG no leitor
+    (status, TagType) = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
+    # Leitura da TAG
+    if status == LeitorRFID.MI_OK:
+        print('TAG Detectada!')
+        (status, uid) = LeitorRFID.MFRC522_Anticoll()
+        uid = tuple(uid)
+
+        if uid in tags_liberadas.keys()
+            print('ID: {} - Acesso Liberado'.format(tags_liberadas[uid]))
+        else
+            print('ID: {} - Acesso Bloqueado'.format(tags_liberadas[uid]))
+
+
+
+        print('uid: ', uid)
+    time.sleep(.5)
