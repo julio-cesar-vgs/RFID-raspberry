a6de4236aa587bf47052f78cc5b3dcd70d857bd0
DESKTOP-1NTIMEG\Julio <julio.cesar.vgs@gmail.com>
2018-10-27 10:58:22 -0300

    subindo o arquivo consulta_tag.py

---
+++ consulta_tag.py
@@ -0,0 +1,24 @@
+import MFRC522
+import Rpi.GPIO as GPIO
+import time
+# Create an object of the class MFRC522
+LeitorRFID = MFRC522.MFRC522()
+
+# Welcome message
+print("Aproxime a Tag")
+# print("Press Ctrl-C to stop.")
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while True:
+
+    # Verificar se existe tag no leitor
+    (status, TagType) = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
+
+    # Leitura da TAG
+    if status == LeitorRFID.MI_OK:
+        print("Cartão detectado")
+        # Get the UID of the card
+        (status, uid) = LeitorRFID.MFRC522_Anticoll()
+        print('uid', uid)
+
+    time.sleep(5)
