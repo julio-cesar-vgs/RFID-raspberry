8071ba519fcd420e9d054c7bd83aaf364beb0d56
Sleepk <julio.cesar.vgs@gmail.com>
2018-10-28 16:05:34 -0300

    criado um arquivo para acesso remoto

--- rfid_remoto.py
+++ rfid_remoto.py
@@ -1,47 +1,67 @@
 from boto3.dynamodb.conditions import Key
 import boto3
+import datetime
 import MFRC522
 import RPi.GPIO as GPIO
 import time
 
-
 dynamodb = boto3.resource('dynamodb')
-
-tabelaTag = dynamodb.Table('tags')
+tabelaTags = dynamodb.Table('tags')
+tabelaLogs = dynamodb.Table('logs')
 
 LeitorRFID = MFRC522.MFRC522()
-print('Aproxime a TAG')
+
 
 def le_tag():
-        while True:
-            # Verifica se existe TAG no leitor
-            (status, TagType) = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
-            # Leitura da TAG
-            if status == LeitorRFID.MI_OK:
-                print('TAG Detectada!')
-                (status, uid) = LeitorRFID.MFRC522_Anticoll()
-                # pega o id da tag e remove as ,
-                uid = ''.join(str(registro) for registro in uid)
-                print(uid)
-                break
-        return uid
+    while True:
+        # Verifica se existe TAG no leitor
+        (status, TagType) = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
 
+        # Leitura da TAG
+        if status == LeitorRFID.MI_OK:
+            print('TAG Detectada!')
+            (status, uid) = LeitorRFID.MFRC522_Anticoll()
+            uid = ''.join(str(registro) for registro in uid)
+            break
+    return uid
 
-# consulta o registro
 def consulta_db(uid):
-    # cria uma consulta
-    resultado_consulta = tabelaTag.query(
-        #busca pela chave
+    resultado_consulta = tabelaTags.query(
         KeyConditionExpression=Key('id').eq(uid)
     )
-    print(resultado_consulta)
+    #print(resultado_consulta)
     return resultado_consulta
+
+
+def valida_tag(resultado_consulta):
+    if len(resultado_consulta['Items'])==1:
+        usuario = resultado_consulta['Items'][0]['usuario']
+        print('Usuario: {} - Acesso Liberado!'.format(usuario))
+        acesso = 'Liberado'
+    else:
+        print('Usuario: Inválido - Acesso Negado!')
+        acesso = 'Negado'
+    return acesso
+
+
+def registra_acesso(uid,acesso):
+    timestamp = str(datetime.datetime.now())
+    tabelaLogs.put_item(
+            Item={
+                'timestamp': timestamp,
+                'id': uid,
+                'acesso': acesso
+            }
+        )
 
 
 try:
     while True:
         uid = le_tag()
         resultado_consulta = consulta_db(uid)
+        acesso = valida_tag(resultado_consulta)
+        registra_acesso(uid,acesso)
         time.sleep(.5)
-except  KeyboardInterrupt:
+except KeyboardInterrupt:
     GPIO.cleanup()
+
